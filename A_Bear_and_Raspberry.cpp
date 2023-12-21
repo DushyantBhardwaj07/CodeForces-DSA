8954662ad9aceb8b011b55fd2c597dab5d53560a
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2023-12-21 18:24:00 +0530

    Add A_Bear_and_Raspberry.cpp

---
+++ A_Bear_and_Raspberry.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+ll solve(){
+    ll n, c; cin>>n>>c;
+    vector<ll>v(n, 0);
+    for(int i=0;i<n;i++) cin>>v[i];
+
+    ll ans = 0;
+    for(int i=0;i<n-1;i++){
+        ll j = i + 1;
+        if(v[i] > v[j]){
+            ans = max(ans, v[i] - v[j] - c);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout<<solve()<<endl;
+    return 0;
+}
