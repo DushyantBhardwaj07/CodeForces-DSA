7df8ffae7609e874651e2e887160f9a874d2cf04
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2023-12-26 18:11:40 +0530

    Add A_Cheap_Travel.cpp

---
+++ A_Cheap_Travel.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+ll solve(){
+    ll n, m, a, b; cin>>n>>m>>a>>b;
+    ll ans = 0;
+    ll temp1 =INT_MAX;
+    if(a < b){
+        ll n1 = n;
+        while(n1>0){
+            ans = ans + a;
+            n1--;
+        } 
+        temp1 = ans;   
+    }
+    
+    ans = 0;
+    while(n>=m){
+        n = n - m;
+        ans = ans + b;
+    }
+    ll temp = ans + b;
+    while(n>0){
+        ans = ans + a;
+        n--;
+    }   
+
+    return min({temp, ans, temp1});
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout<<solve()<<endl;
+    return 0;
+}
