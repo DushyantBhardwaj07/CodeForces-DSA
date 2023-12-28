0f375ffe2486bf8a266a83fd00fab7260c3c5934
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2023-12-28 17:38:04 +0530

    Add A_Cifera.cpp

---
+++ A_Cifera.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+void solve(){
+    ll k,l; cin>>k>>l;
+    ll c = 0;
+    while(l%k == 0){
+        l = l / k;
+        c++;
+    }
+    if(l == 1){
+        cout<<yes<<endl;
+        cout<<c-1;
+        return;
+    }
+    cout<<no<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    solve();
+    return 0;
+}
