69d16d5cd8cd00f55a89f0eaaa3e3860c70d375c
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2023-12-31 10:23:14 +0530

    Add A_Cover_in_Water.cpp

---
+++ A_Cover_in_Water.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+ll solve(){
+    ll n; cin>>n;
+    string s; cin>>s;
+    ll c = 0;
+    for(auto it :s){
+        if(it == '.') c++;
+    }
+    if(s.find("...") != -1){
+        return 2;
+    }
+
+    bool flag = true;
+    for(int i=0;i<n;i++){
+        if(s[i] == '.'){
+            flag = false;
+            break;
+        }
+    }
+    if(flag){
+        return 0;
+    }
+
+    return c;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        cout<<solve()<<"\n";
+    }
+    return 0;
+}
