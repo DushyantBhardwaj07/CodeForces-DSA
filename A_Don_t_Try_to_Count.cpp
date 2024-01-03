18105f89ef5702a8f4f4740c6528c53705867fc2
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-01-03 10:16:36 +0530

    Add A_Don_t_Try_to_Count.cpp

---
+++ A_Don_t_Try_to_Count.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+ll solve(){
+    ll n, m; cin>>n>>m;
+    string x; cin>>x;
+    string s; cin>>s;
+    ll t = 10;
+    ll c = 0;
+    while(t--){
+        if(x.find(s) != -1){
+            return c;
+        }
+        c++;
+        x = x + x;
+    }
+    return -1;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        cout<<solve()<<"\n";
+    }
+    return 0;
+}
