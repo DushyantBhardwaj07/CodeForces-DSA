d2670467b42f2d6d9c244f92b277acb870e9faf4
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-01-05 09:24:07 +0530

    Add A_Drazil_and_Date.cpp

---
+++ A_Drazil_and_Date.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "Yes"
+#define no "No"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+string solve(){
+    ll a,b,s; cin>>a>>b>>s;
+    ll temp = abs(a - 0) + abs(b-0);
+    if(a == 0 && b == 0 && s%2 == 0) return yes; 
+    if(temp == s) return yes;
+    else if(temp < s && ((s-temp) %2 == 0)) return yes;
+    return no;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout<<solve()<<endl;
+    return 0;
+}
