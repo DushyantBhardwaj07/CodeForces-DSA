9706280abd9f1d919317844d4a8e59e29b4ac1c6
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-01-07 09:52:18 +0530

    Add A_Escape_from_Stones.cpp

---
+++ A_Escape_from_Stones.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+void solve(){
+    string s; cin>>s;
+    vector<ll>first;
+    vector<ll>second;
+    for(int i=0;i<s.length();i++){
+        if(s[i] == 'r') first.push_back(i+1);
+        else second.push_back(i+1);
+    }
+    reverse(second.begin(), second.end());
+
+    for(int i=0;i<first.size();i++){
+        cout<<first[i]<<"\n";
+    }
+    for(int i=0;i<second.size();i++){
+        cout<<second[i]<<"\n";
+    }
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    solve();
+    return 0;
+}
