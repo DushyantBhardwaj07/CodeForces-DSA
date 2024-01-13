e4227abaf752d20c33619778b9b634b24a400671
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-01-13 10:11:04 +0530

    Add A_Fox_and_Number_Game.cpp

---
+++ A_Fox_and_Number_Game.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<ll>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+ll solve(){
+    ll n; cin>>n;
+    priority_queue<int>maxheap;
+        
+    vector<ll>v(n,0);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    ll gc = v[0];
+    for(int i=1;i<n;i++){
+        gc = __gcd(gc, v[i]);
+    }
+    return gc*n;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout<<solve()<<endl;
+    return 0;
+}
