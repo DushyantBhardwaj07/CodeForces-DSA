8b2a75f8de44d07e3c37bbd54b4497db8859428d
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-02-12 10:00:34 +0530

    Add A_Polycarp_and_Sums_of_Subsequences.cpp

---
+++ A_Polycarp_and_Sums_of_Subsequences.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+void solve(){
+    vector<ll>v(7,0);
+    for(int i=0;i<7;i++){
+        cin>>v[i];
+    }
+    cout<<v[0]<<" "<<v[1]<<" "<<v.back() - v[0] - v[1]<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        solve();    
+        // cerr<<"Time: "<<(double)clock()/CLOCKS_PER_SEC<<" s\n";
+
+    }
+    return 0;
+}
