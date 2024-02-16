acd7aa06c41db5debc8d4d8c99efc808b86c100b
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-02-16 12:35:35 +0530

    Add A_Red_Versus_Blue.cpp

---
+++ A_Red_Versus_Blue.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+void solve(){    
+    int n,r,b;
+    cin>>n>>r>>b;
+    int p=r/(b+1),q=r%(b+1);
+    for(int i=0;i<q;i++)    cout<<string(p+1,'R')<<'B';
+    for(int i=q;i<b;i++)    cout<<string(p,'R')<<'B';
+    cout<<string(p,'R');
+    cout<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        solve();
+    }
+    return 0;
+}
