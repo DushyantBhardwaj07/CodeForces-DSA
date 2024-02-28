d6eecf7ce1f5356d0059ce431c211d40dc770c2b
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-02-28 15:04:02 +0530

    Add A_Sum_of_Round_Numbers.cpp

---
+++ A_Sum_of_Round_Numbers.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<string>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+void solve(){
+    string s; cin>>s;
+    ll n = s.length();
+    vector<string>v;
+    for(int i=0;i<s.length();i++){
+        if(s[i] != '0'){
+            char first = s[i];
+            string num = first + string(n-(i+1), '0');
+            v.push_back(num);
+        }
+    }
+    cout<<v.size()<<endl;
+    display(v);
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        solve();
+    }
+    return 0;
+}
