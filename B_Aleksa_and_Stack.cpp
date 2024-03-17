2f4a49b3ad08089089b0461eeafbc2b7112427b2
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-03-17 16:48:41 +0530

    Add B_Aleksa_and_Stack.cpp

---
+++ B_Aleksa_and_Stack.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+void solve(){
+    ll n; cin>>n;
+    vector<ll>ans;   
+    ans.push_back(2);
+    ans.push_back(3);
+    n = n -2;
+    for(int i=0;i<n;i++){
+        ll temp = ans[ans.size() -1] + ans[ans.size()-2];
+        ll j = ans.back() + 1;
+        while(true){
+            if(j*3 % temp != 0){
+                ans.push_back(j);
+                break;
+            }
+            j++;
+        }
+
+    }
+
+    display(ans);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        solve();
+    }
+    return 0;
+}
