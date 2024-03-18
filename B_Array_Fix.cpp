ddc6fc8967ca59897841991bd762189541cfc57f
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-03-18 14:03:19 +0530

    Add B_Array_Fix.cpp

---
+++ B_Array_Fix.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+string solve(){
+    ll n; cin>>n;
+    vector<ll>v(n,0);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+
+    // for(int i=n-1;i>0;i--){
+    //     if(v[i-1] > v[i]){
+    //         if(((v[i-1])%10 > v[i])){
+    //             return no;
+    //         }
+    //         else if(((v[i-1])%10 < v[i]) && (((v[i-1])%10 < (v[i-1]) / 10))){
+    //             return no;
+    //         }
+    //     }
+    // }
+
+    stack<ll>s;
+    for(int i=n-1;i>=0;i--){
+        if(s.empty()){
+            s.push(v[i]);
+        }
+        else if(s.top() >= v[i]){
+            s.push(v[i]);
+        }
+        else if(s.top() < v[i]){
+            if(s.top() < v[i]%10){
+                return no;
+            }
+            else if(s.top() >= v[i]%10 && (v[i]/10) > (v[i]%10)){
+                return no;
+            }
+            else if(s.top() >= v[i]%10 && (v[i]/10) <= (v[i]%10)){
+                s.push(v[i]%10);
+                s.push(v[i]/10);
+            }
+
+        }
+    }
+    return yes;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        cout<<solve()<<"\n";
+    }
+    return 0;
+}
