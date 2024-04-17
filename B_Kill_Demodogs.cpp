858656fe243dcc80a8a686f4e6dda77fc7fdaea5
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-04-17 10:29:18 +0530

    Add B_Kill_Demodogs.cpp

---
+++ B_Kill_Demodogs.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+// #define mod 1e9+7
+int mod = 1e9+7;
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+ll solve(){
+    ll n; cin>>n;
+    ll temp = ((n%mod*(n+1)%mod*(2*n + 1)%mod)%mod * 337)%mod;
+    n--;
+    ll temp1 = ((n%mod*(n+1)%mod*(2*n + 1)%mod)%mod * 337)%mod+ ((n%mod*(n+1)%mod)%mod * 1011)%mod;
+    ll summ = (temp1%mod + temp%mod)%mod;
+    return summ;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        cout<<solve()<<"\n";
+    }
+    return 0;
+}
+ 
