30b9864620bd0afd9cedeec92efefd26983ef48c
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-04-18 12:45:46 +0530

    Add B_Letters_Shop.cpp

---
+++ B_Letters_Shop.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+void solve(){
+    ll n,m; cin>>n;
+    string s,t; cin>>s;
+    map<char, vector<ll>>main;
+    for(int i=0;i<n;i++){
+        main[s[i]].push_back(i+1);
+    }
+    cin>>m;
+    for(int i=0;i<m;i++){
+        cin>>t;
+        ll maxi = INT_MIN;
+        map<char,ll>mapt;
+        for(int i=0;i<t.length();i++){
+            mapt[t[i]]++;
+        }
+        for(auto it: mapt){
+            maxi = max(maxi, main[it.first][it.second-1]);
+        }
+        cout<<maxi<<endl;
+    }
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    solve();
+    return 0;
+}
