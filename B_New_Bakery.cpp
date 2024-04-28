74baf14bd21bef4972f01c0476a52c4fd6a5f35c
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-04-28 10:08:09 +0530

    Add B_New_Bakery.cpp

---
+++ B_New_Bakery.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+ll solve(){
+    ll n,a,b; cin>>n>>a>>b;
+    // k(b-i + 1) + (n-k)a;
+    ll t = max(0LL, min(b-a+1, n));
+    ll temp = (ll)((t * (t+1))/(ll)2);
+    ll ans = (t*b) - temp + (n-t) *a + t;   
+    return ans;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        cout<<solve()<<"\n";
+    }
+    return 0;
+}
