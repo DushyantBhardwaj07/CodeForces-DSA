afa4813dff20dccba5caa4f039379e0df89cf5b0
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-05-11 10:54:42 +0530

    Add B_Roof_Construction.cpp

---
+++ B_Roof_Construction.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+void solve(){
+    ll n; cin>>n;
+    n =n-1;
+    ll power2 = 0;
+    for(int i=n;i>0;i--){
+        if(!(i & (i-1))){
+            power2 = i;
+            break;
+        }
+    }
+    // cout<<power2<<endl;
+    for(int i=n;i>=power2;i--){
+        cout<<i<<" ";
+    }
+    cout<<0<<" ";
+    for(int i=power2-1;i>0;i--){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        solve();
+    }
+    return 0;
+}
