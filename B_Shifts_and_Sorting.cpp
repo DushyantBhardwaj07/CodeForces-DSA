a2e8355e99aa886eccb7f6c6eb2a7b3a9bea3a05
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-05-12 14:33:25 +0530

    Add B_Shifts_and_Sorting.cpp

---
+++ B_Shifts_and_Sorting.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+ll solve(){
+    string s; cin>>s;
+    ll start = -1;
+    for(int i=0;i<s.length();i++){
+        if(s[i] == '1'){
+            start = i;
+            break;
+        }
+    }
+    
+    if(start == -1) return 0;
+
+    ll ans = 0;
+    for(int i=start;i<s.length();i++){
+        if(s[i] == '0'){
+            ans = ans + i-start+1;
+            start++;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        cout<<solve()<<"\n";
+    }
+    return 0;
+}
