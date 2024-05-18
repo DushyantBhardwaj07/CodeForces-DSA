82a1400976221519a7016a27c77d06b04e89b13d
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-05-18 18:02:46 +0530

    Add B_Three_Threadlets.cpp

---
+++ B_Three_Threadlets.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+string solve(){
+    ll a,b,c; cin>>a>>b>>c;
+    if(a == b && a == c) return yes;
+    vector<ll>v = {a,b,c};
+    sort(v.begin(), v.end());
+    a = v[0];
+    b = v[1]; c = v[2];
+    if(b%a !=0 || c %a !=0) return no;
+    ll ans = ( b/ a) + (c / a) -2;
+    if(ans > 3) return no;
+    return yes;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        cout<<solve()<<"\n";
+    }
+    return 0;
+}
