514cf17732a73c7361dffc3d3f2bae5e08376f13
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-06-05 15:45:22 +0530

    Add C_Clock_Conversion.cpp

---
+++ C_Clock_Conversion.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+string solve(){
+    string s; cin>>s;
+    ll hour = stoi(s.substr(0, 2));
+    string min = s.substr(3, 2);
+    string ap;
+    if(hour < 12){
+        ap = "AM";
+        if(hour == 0) hour = 12;
+    }
+    else{
+        ap = "PM";
+    }
+
+    if(hour > 12){
+        hour = hour-12;        
+    }
+
+    string ans = to_string(hour);
+    if(ans.length() == 1) ans = "0"+ans;
+
+    return ans+":"+min+" "+ap;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        cout<<solve()<<"\n";
+    }
+    return 0;
+}
