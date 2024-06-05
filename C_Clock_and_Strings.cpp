a8d5192933f264fd0fc1c1b3a72a5df88bc553b4
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-06-05 17:04:43 +0530

    Add C_Clock_and_Strings.cpp

---
+++ C_Clock_and_Strings.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+string solve(){
+    vector<ll>v(4,0);
+    for(int i=0;i<4;i++){
+        cin>>v[i];
+    }
+    string str = "";
+    for(int i=1;i<=12;i++){
+        if(i == v[0] || i == v[1]){
+            str = str  + "r";
+        }
+        else if(i == v[2] || i == v[3]){
+            str = str + "b";
+        }
+    }
+    if(str == "rbrb" || str == "brbr"){
+        return yes;
+    }
+    return no;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        cout<<solve()<<"\n";
+    }
+    return 0;
+}
