5cdae3478e5521c159b1b8d7be8d26e4fe133b9e
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-06-06 11:25:38 +0530

    Add C_Cypher.cpp

---
+++ C_Cypher.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+void solve(){
+    ll n; cin>>n;
+    vector<ll>v(n,0);
+    for(int i=0;i<n;i++) cin>>v[i];
+    vector<ll>ans;
+    ll l; string s;
+    for(int i=0;i<n;i++){
+        cin>>l;
+        cin>>s;
+        for(int j=0;j<s.length();j++){
+            // cout<<v[i]<<endl;
+            if(s[j] == 'D'){
+                v[i] = (v[i]+1)%10;
+            }
+            else if(s[j] == 'U'){
+                if((v[i]-1)< 0){
+                    v[i] =  10 - abs(v[i]-1)%10;
+                }
+                else{
+                    v[i] =  (v[i]-1)%10;
+                }
+                
+            }
+        }
+    }
+    display(v);
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        solve();
+    }
+    return 0;
+}
