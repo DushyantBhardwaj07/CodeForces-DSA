fd55af6ca9c9768422003b02fe58f3ca683fa9eb
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-06-09 16:29:03 +0530

    Add C_Exams.cpp

---
+++ C_Exams.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+ll solve(){
+    ll n; cin>>n;
+    vector<pair<ll,ll>>v;
+    ll a,b;
+    for(int i=0;i<n;i++){
+        cin>>a>>b;  
+        v.push_back({a,b});
+    }
+    sort(v.begin(), v.end());
+    ll prev = 0;
+    for(int i=0;i<v.size();i++){
+        if((v[i].first < v[i].second) && (v[i].first >= prev)){
+            prev = v[i].first;
+        }
+        else if((v[i].first > v[i].second) && (v[i].second >= prev)){
+            prev = v[i].second;
+        }
+        else if((v[i].first >= prev)){
+            prev = v[i].first;
+        }
+        else{
+            prev = v[i].second;
+        }
+    }
+    return prev;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout<<solve()<<endl;
+    return 0;
+}
