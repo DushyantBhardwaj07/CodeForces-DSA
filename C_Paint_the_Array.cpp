703db567a8bc2a96df012a2a21917d67cedcc55e
Dushyant Rajeev Bhardwaj <dushyantbhardwaj26@gmail.com>
2024-06-24 17:28:11 +0530

    Add C_Paint_the_Array.cpp

---
+++ C_Paint_the_Array.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes "YES"
+#define no "NO"
+#define ll long long
+#define mod 1000000007
+void display(vector<ll>v){ for(ll i=0;i<v.size();i++) cout<<v[i]<<' ';cout<<endl;}
+void display2d(vector<vector<ll>> v){for(ll i=0;i<v.size();i++){for(ll j=0;j<v[i].size();j++){cout<<v[i][j]<<' ';}cout<<endl;}cout<<endl;}
+ll add(vector<int>v){int sum = 0;for(int i=0;i<v.size();i++)sum = sum+v[i];return sum;}
+bool isOdd(int n){if(n&1) return true;return false;}
+bool isPrime(int n){if (n <= 1)return false;for (int i = 2; i <= n / 2; i++)if (n % i == 0)return false;return true;}
+bool isVowel(char a){if(a=='a'||a=='e'||a == 'i'||a=='o'||a=='u'||a=='y'){return true;}return false;}
+
+ll solve(){
+    ll n; cin>>n;
+    vector<ll>v(n,0);
+    for(int i=0;i<n;i++) cin>>v[i];
+    ll first =0;
+    for(int i=0;i<n;i=i+2){
+        first = __gcd(first, v[i]);
+    }
+    ll second =0;
+    for(int i=1;i<n;i=i+2){
+        second = __gcd(second, v[i]);
+    }
+    bool flag = true;
+    for(int i=1;i<n;i = i+2){
+        if(v[i] % first == 0){
+            flag = false;
+            break;
+        }
+    }
+    if(flag) return first;
+    
+    flag = true;
+    for(int i=0;i<n;i = i+2){
+        if(v[i] % second == 0){
+            flag = false;
+            break;
+        }
+    }
+    if(flag) return second;
+
+    return 0;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    ll t;cin>>t;
+    while(t-->0){
+        cout<<solve()<<"\n";
+    }
+    return 0;
+}
